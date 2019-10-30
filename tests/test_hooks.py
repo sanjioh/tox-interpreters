7cbb50d9e3f9ea6a2e884aa5aca8633503cc0d84
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-10-30 12:07:14 +0100

    improve hook tests

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,12 +1,31 @@
-from tox_interpreters.hooks import tox_get_python_executable
+from unittest import mock
+
+from tox import reporter
+
+from tox_interpreters.hooks import tox_addoption, tox_get_python_executable
 
 
-def test_python_executable_found(envconfig):
+def test_config_option():
+    parser = mock.Mock()
+    tox_addoption(parser)
+    parser.add_testenv_attribute.assert_called_once()
+
+
+def test_python_executable_found(capfd, envconfig):
     interpreter = '/path/to/python'
     envconfig.interpreter = interpreter
+    reporter._INSTANCE._reset()  # pick up pytest monkeypatching of stdout
     assert tox_get_python_executable(envconfig) == interpreter
+    captured = capfd.readouterr()
+    assert 'pythonenv base interpreter: /path/to/python' in captured.out
 
 
-def test_python_executable_not_found(envconfig):
+def test_python_executable_not_found(capfd, envconfig):
     envconfig.interpreter = None
+    reporter._INSTANCE._reset()  # pick up pytest monkeypatching of stdout
     assert tox_get_python_executable(envconfig) is None
+    captured = capfd.readouterr()
+    assert (
+        'pythonenv base interpreter: delegating to standard resolution'
+        in captured.out
+    )
